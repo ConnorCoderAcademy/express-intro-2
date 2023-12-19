161ee662595941a2744c52cfa388722712c7ceb9
ConnorCoderAcademy <13716@coderacademy.edu.au>
2023-12-19 20:34:00 +1100

    post request

--- index.js
+++ index.js
@@ -7,16 +7,20 @@ const app = express();
 
 const HOST = process.env.HOST || "localhost"
 const PORT = process.env.PORT || 3000;
-// ports run in intergers between 1001 - 65536
+//ports run in integers between 1001 - 65536
 
 // write the routes
-// GET localhost:3000/
+//GET localhost:3000/
 // app.get(route path, callback function to handle request and response)
-
-app.get("/", (request, response) => {
-    response.send("Hello World, server building in progress")
-
+app.get('/', (request, response) => {
+    response.send("Hello world, server building in progress...");
 })
+
+const pokemonRouter = require('./controllers/PokemonController');
+app.use("/pokemon", pokemonRouter);
+
+
+
 
 // Configure the server
 
